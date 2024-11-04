f6afffd807db0fafd6f869f13942d8b26d69aece
Ryan210 <s1121126@o365st.pu.edu.tw>
2024-11-04 15:43:31 +0800

    v4_密碼輸入

--- app/src/main/java/com/example/s1121126/MainActivity.kt
+++ app/src/main/java/com/example/s1121126/MainActivity.kt
@@ -47,6 +47,7 @@ class MainActivity : ComponentActivity() {
 }
 @Composable
 fun Birth(m: Modifier) {
+    var userPassword by remember { mutableStateOf("")}
     var userName by remember { mutableStateOf("吳容陞") }
     var userWeight by remember { mutableStateOf(3800) }
 
@@ -72,9 +73,20 @@ fun Birth(m: Modifier) {
             keyboardOptions = KeyboardOptions
                 (keyboardType = KeyboardType.Number)
         )
+        TextField(
+            value = userPassword,
+            onValueChange = { newText ->
+                userPassword = newText
+            },
+            label = { Text("密碼") },
+            placeholder = { Text(text = "請輸入您的密碼") },
+            visualTransformation = PasswordVisualTransformation(),
+            keyboardOptions = KeyboardOptions
+                (keyboardType = KeyboardType.Password)
+        )
 
 
-        Text("您輸入的姓名是：$userName\n出生體重為：$userWeight 公克")
+        Text("您輸入的姓名是：$userName\n出生體重為：$userWeight 公克"+ "\n密碼：$userPassword")
     }
 
 }
